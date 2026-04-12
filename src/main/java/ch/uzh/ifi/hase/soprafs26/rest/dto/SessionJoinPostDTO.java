e930400a793f673160a925e8f53f15365ae79c15
Enrique G Zbinden <enriquezbinden04@gmail.com>
2026-04-12 13:43:28 +0200

    #27 Add join session endpoint and validation

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/rest/dto/SessionJoinPostDTO.java
@@ -0,0 +1,14 @@
+package ch.uzh.ifi.hase.soprafs26.rest.dto;
+
+public class SessionJoinPostDTO {
+
+    private Long userId;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
